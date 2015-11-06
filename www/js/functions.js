d4791898fd06b56fcd0477650acba1616200fb27
vmoore-paris <virginie.moore+github@gmail.com>
2015-11-06 09:57:08 +0100

    debug

--- www/js/functions.js
+++ www/js/functions.js
@@ -140,15 +140,25 @@ function createHorairesSuccess(callback,$interval,$scope)
 			console.log(xhr_object);
 			console.log(xhr_object.response);
 			var MyHoraires = JSON.parse(xhr_object.response);
+			if (debug || debug_loadDB)
+				alertDebug("var MyHoraires");
 			db.transaction(function(tx) 
 			{  
+				if (debug || debug_loadDB)
+					alertDebug("transaction");
 				for(var k in MyHoraires) {
+						if (debug || debug_loadDB)
+							alertDebug("for");
 					   console.log(k, MyHoraires[k]);
 					   console.log('SELECT COUNT("id") as cnt FROM "horaires" WHERE uidquestionnaire = "'+MyHoraires[k].sid+'" AND tsdebut = '+MyHoraires[k].ts+'');
 					   (function (value) { 
 					   tx.executeSql('SELECT COUNT("id") as cnt  FROM "horaires" WHERE uidquestionnaire = "'+MyHoraires[k].sid+'" AND tsdebut = '+MyHoraires[k].ts+';', [], function(tx, res) {
+						   if (debug || debug_loadDB)
+								alertDebug("SELECT");
 						   if (res.rows.item(0).cnt < 1)
 						   {
+							   f (debug || debug_loadDB)
+								alertDebug("if SELECT");
 							   tx.executeSql('INSERT INTO "horaires" (uidquestionnaire, tsdebut,fait) VALUES("'+
 									   value.sid+'","'+
 									   value.ts+'",0);',[], successHandler, errorHandler);
