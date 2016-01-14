2dac8ae267a806f2e0308ed1bf631560136ae056
vmoore-paris <virginie.moore+github@gmail.com>
2016-01-14 10:09:14 +0100

    #44 correction url extérieure

--- www/js/demo.js
+++ www/js/demo.js
@@ -390,7 +390,8 @@ app.controller('MainController', function(cordovaReady,$rootScope, $scope,$locat
 			        	{
 			        		cordova.plugins.email.open({
 			        			subject: 'rapport données',
-			        		    attachments: entry.toURL() //=> res/drawable/icon (Android)
+			        		    //attachments: entry.toURL() //=> res/drawable/icon (Android)
+			        			attachments: fileURL //=> res/drawable/icon (Android)
 			        		});
 			        	}
 			        	else
